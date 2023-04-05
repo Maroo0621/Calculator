7562afb3e5b0deb3b32e37dfa33d156385d1e47c
maroo <kylua567@gmail.com>
2023-04-05 13:59:14 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,100 @@
+const numbers = document.querySelectorAll('.number');
+const result = document.querySelector('.result');
+const signs = document.querySelectorAll('.operator');
+const equals = document.querySelector('.equal-sign')
+const clear = document.querySelector('.all-clear');
+const percent = document.querySelector('.percentage')
+
+let firstValue = "";
+let isFirstValue = false;
+let secondValue = "";
+let isSecondValue = false;
+let sign = "";
+let resultValue = 0;
+
+for(let i = 0; i < numbers.length; i++) {
+  numbers[i].addEventListener('click', (e) => {
+    let atr = e.target.getAttribute('value');
+    if(isFirstValue === false) {
+      getFirstValue(atr)
+    }
+    if(isSecondValue == false) {
+      getSecondValue(atr);
+    }
+  })
+}
+
+function getFirstValue(el) {
+  result.innerHTML = "";
+  firstValue += el;
+  result.innerHTML = firstValue;
+  firstValue = +firstValue;
+}
+
+function getSecondValue(el) {
+  if(firstValue != "" && sign != "") {
+    secondValue += el;
+    result.innerHTML = secondValue;
+    secondValue = +secondValue;
+  }
+}
+
+function getSign() {
+  for(let i = 0; i < signs.length; i++) {
+    signs[i].addEventListener('click', (e) => {
+      sign = e.target.getAttribute('value');
+      isFirstValue = true
+    })
+  }
+} 
+getSign()
+
+equals.addEventListener('click', () => {
+  result.innerHTML = "";
+  if(sign === "+") {
+    resultValue = firstValue + secondValue
+  } else if (sign === "-") {
+    resultValue = firstValue - secondValue
+  } else if (sign === "*") {
+    resultValue = firstValue * secondValue
+  } else if (sign === "/") {
+    resultValue = firstValue / secondValue
+  } 
+  result.innerHTML = resultValue;
+  firstValue = resultValue;
+  secondValue = "";
+
+  checkResultLength()
+})
+
+function checkResultLength() {
+  resultValue = JSON.stringify(resultValue);
+
+  if(resultValue.length >= 8) {
+    resultValue = JSON.parse(resultValue);
+    result.innerHTML = resultValue.toFixed(5);
+  }
+}
+
+percent.addEventListener('click', () => {
+  result.innerHTML = "";
+  if(firstValue != "") {
+    resultValue = firstValue / 100;
+    firstValue = resultValue;
+  }
+  if(firstValue != "" && secondValue != "" && sign != "") {
+    resultValue = resultValue / 100
+  }
+  result.innerHTML = resultValue;
+})
+
+clear.addEventListener('click', () => {
+  result.innerHTML = 0;
+
+  firstValue = "";
+  isFirstValue = false;
+  secondValue = "";
+  isSecondValue = false;
+  sign = ""
+  resultValue = 0
+})
